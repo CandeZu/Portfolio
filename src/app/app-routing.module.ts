cbd89dc7a70d49cfa88c3175e765365ca9e56423
Candela Zumel <candelazumel@gmail.com>
2022-05-30 11:09:03 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobremiComponent } from './pages/sobremi/sobremi.component';
+import { EstudiosComponent } from './pages/estudios/estudios.component';
+import { AptitudesComponent } from './pages/aptitudes/aptitudes.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: SobremiComponent},
+  {path:'estudios', component: EstudiosComponent},
+  {path:'aptitudes', component: AptitudesComponent},
+  {path:'contacto', component: ContactoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SobremiComponent } from './pages/sobremi/sobremi.component';
+import { EstudiosComponent } from './pages/estudios/estudios.component';
+import { AptitudesComponent } from './pages/aptitudes/aptitudes.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SobremiComponent,
+    EstudiosComponent,
+    AptitudesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
